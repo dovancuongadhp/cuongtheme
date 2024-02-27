5cfd29a0dff7883aade65fb4643f0653bdb3c1e3
Do Van Cuong <61376768+dovancuongadhp@users.noreply.github.com>
2024-02-27 16:12:50 +0700

    Update content.php

--- content.php
+++ content.php
@@ -1,12 +1,12 @@
-<article id="post-<?php the_ID()?>" <?php post_class()?>>
-<div class="entry-thumbnail">
-    <?php cuong_theme_thumbnail('thumbnail')?>
-</div>
-<div class="entry-header">
+<article id="post-<?php the_ID() ?>" <?php post_class() ?>>
     <div style="background-color: bisque; margin: 10px; padding: 20px">
-        <?php cuong_theme_entry_header() ?>
-        <?php cuong_theme_entry_meta() ?>
+        <div class="entry-thumbnail">
+            <?php cuong_theme_thumbnail('thumbnail') ?>
+        </div>
+        <div class="entry-header">
+            <?php cuong_theme_entry_header() ?>
+            <?php cuong_theme_entry_meta() ?>
+        </div>
     </div>
-</div>
-<div class="entry-content"></div>
+    <div class="entry-content"></div>
 </article>
