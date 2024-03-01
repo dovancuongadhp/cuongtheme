70f8d776f7b0ae80d03f0520dd6806faeb66febb
Do Van Cuong <61376768+dovancuongadhp@users.noreply.github.com>
2024-03-01 14:45:46 +0700

    fix href post format link

--- content-link.php
+++ content-link.php
@@ -5,12 +5,12 @@
                 $link = get_post_meta($post->ID, 'format_link_url', true);
                 $link_description = get_post_meta($post->ID, 'format_link_description', true);
                 if(is_single()){
-                    printf('<h1 class="entry-title"><a href="$1$s" target="blank">%2$s</a></h1>',
+                    printf('<h1 class="entry-title"><a href="%1$s" target="blank">%2$s</a></h1>',
                         $link,
                         get_the_title()
                     );
                 }else{
-                    printf('<h2 class="entry-title"><a href="$1$s" target="blank">%2$s</a></h2>',
+                    printf('<h2 class="entry-title"><a href="%1$s" target="blank">%2$s</a></h2>',
                     $link,
                     get_the_title()
                 );
