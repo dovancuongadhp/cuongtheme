cc6eec19cc2ee48bb0a0a39382f40a43c019895b
Do Van Cuong <61376768+dovancuongadhp@users.noreply.github.com>
2024-03-02 18:01:11 +0700

    search and 404

---
+++ search.php
@@ -0,0 +1,32 @@
+<?php 
+get_header(); 
+?>
+<div class="content">
+    <div class="main-content">
+        <div class="search-info">
+            <?php 
+                $search_query = new WP_Query('s='.$s.'&showpost=-1');
+                $search_keyword = _wp_specialchars($s,1);
+                $search_count = $search_query->post_count;
+                printf(__('We found %1$s article for your search','cuongtheme'), $search_count);
+            ?>
+        </div>
+        <?php 
+        if (have_posts()) : 
+            while (have_posts()) : 
+                the_post(); 
+                get_template_part('content', get_post_format()); 
+            endwhile; 
+            cuong_theme_pagination(); 
+        else : 
+            get_template_part('content', 'none'); 
+        endif; 
+        ?>
+    </div>
+    <div id="sidebar">
+        <?php get_sidebar()?>
+    </div>
+</div>
+<?php 
+get_footer(); 
+?>
