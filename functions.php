775c419e1fe324dba62b9c85246f5962ca1dc19c
Do Van Cuong <61376768+dovancuongadhp@users.noreply.github.com>
2024-03-05 22:31:12 +0700

    add superfish (dropdown menu)

--- functions.php
+++ functions.php
@@ -94,7 +94,8 @@ if (!function_exists('cuong_theme_menu')) {
 		$menu = array(
 			'theme_location' => $menu,
 			'container' => 'nav',
-			'container_class' => $menu
+			'container_class' => $menu,
+			'items_wrap' => '<ul id="%1$s" class="%2$s sf-menu">%3$s</ul>'
 		);
 		wp_nav_menu($menu);
 	}
@@ -219,5 +220,16 @@ function cuong_theme_style(){
 
 	wp_register_style('reset-style',get_template_directory_uri()."/reset.css",'all');
 	wp_enqueue_style('reset-style');
+
+	// superfish menu
+	wp_register_style('superfish-style',get_template_directory_uri()."/superfish.css",'all');
+	wp_enqueue_style('superfish-style');
+
+	wp_register_script('superfish-script',get_template_directory_uri()."/superfish.js",array('jquery'));
+	wp_enqueue_script('superfish-script');
+
+	// custom script
+	wp_register_script('custom-script',get_template_directory_uri()."/custom.js",array('jquery'));
+	wp_enqueue_script('custom-script');
 }
 add_action('wp_enqueue_scripts', 'cuong_theme_style');
